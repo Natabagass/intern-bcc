bffbea7400427dbf99ade73d2ef2b48ad1eb45b0
Natabagas <meganatabagas@gmail.com>
2023-02-25 16:49:43 +0700

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 import AuthRoute from './routes/AuthRoute';
-import './App.css';
 import Login from './pages/users/Login';
 import Home from './pages/auth/dashboard/Homepage';
 import PrivateRoute from './routes/PrivateRoute';
