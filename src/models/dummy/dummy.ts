70537ae8f9fca2b4b3be6babcc4e5d0f161ff805
Natabagas <meganatabagas@gmail.com>
2023-03-01 14:26:43 +0700

    Page graha

---
+++ src/models/dummy/dummy.ts
@@ -0,0 +1,137 @@
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+
+
+export const gedung = [
+    {
+        id: 1,
+        name: 'Graha Cakrawala',
+        fasilitas: [
+            {
+                id:1,
+                barang: 'bangku'
+            },
+            {
+                id:2,
+                barang: 'meja'
+            },
+            {
+                id:3,
+                barang: 'taplak'
+            },
+            {
+                id:4,
+                barang: 'kaca'
+            },
+        ]
+    },
+    {
+        id: 2,
+        name: 'Graha Cakrawala',
+        fasilitas: [
+            {
+                id:1,
+                barang: 'bangku'
+            },
+            {
+                id:2,
+                barang: 'meja'
+            },
+            {
+                id:3,
+                barang: 'taplak'
+            },
+            {
+                id:4,
+                barang: 'kaca'
+            },
+        ]
+    },
+    {
+        id: 3,
+        name: 'Graha Cakrawala',
+        fasilitas: [
+            {
+                id:1,
+                barang: 'bangku'
+            },
+            {
+                id:2,
+                barang: 'meja'
+            },
+            {
+                id:3,
+                barang: 'taplak'
+            },
+            {
+                id:4,
+                barang: 'kaca'
+            },
+        ]
+    },
+    {
+        id: 4,
+        name: 'Graha Cakrawala',
+        fasilitas: [
+            {
+                id:1,
+                barang: 'bangku'
+            },
+            {
+                id:2,
+                barang: 'meja'
+            },
+            {
+                id:3,
+                barang: 'taplak'
+            },
+            {
+                id:4,
+                barang: 'kaca'
+            },
+        ]
+    },
+    {
+        id: 5,
+        name: 'Graha Cakrawala',
+        fasilitas: [
+            {
+                id:1,
+                barang: 'bangku'
+            },
+            {
+                id:2,
+                barang: 'meja'
+            },
+            {
+                id:3,
+                barang: 'taplak'
+            },
+            {
+                id:4,
+                barang: 'kaca'
+            },
+        ]
+    },
+    {
+        id: 6,
+        name: 'Graha Cakrawala',
+        fasilitas: [
+            {
+                id:1,
+                barang: 'bangku'
+            },
+            {
+                id:2,
+                barang: 'meja'
+            },
+            {
+                id:3,
+                barang: 'taplak'
+            },
+            {
+                id:4,
+                barang: 'kaca'
+            },
+        ]
+    }
+]
--- src/pages/graha/Graha.tsx
+++ src/pages/graha/Graha.tsx
@@ -1,11 +1,44 @@
 import Nav from "../dashboard/Nav";
+import { gedung } from '../../models/dummy/dummy'
+import fotoGedung from '../../assets/gedung.jpg'
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const Graha = () => {
     return (
         <>
             <Nav />
-            <div>
-                INI Page GRAHA
+            <div className="mx-[100px] my-[50px]">
+                <div className="flex flex-row justify-between flex-wrap w-full">
+                    {
+                        gedung.map((data, index) => {
+                            return (
+                                <div key={index} className="mr-[20px] hover:-translate-y-3 transition rounded-xl border my-[30px] bg-white">
+                                    <div>
+                                        <LazyLoadImage className="w-[400px] rounded-t-xl" src={fotoGedung} alt="Foto gedung" />
+                                    </div>
+                                    <div className="bg-slate-200 py-[20px] rounded-b-xl w-full text-gray-500">
+                                        <div className="mx-[20px]">
+                                            <h1 className="text-[18px] font-medium">{data.name}</h1>
+                                            <div>
+                                                {
+                                                    data.fasilitas.map((sub) => {
+                                                        return (
+                                                            <>
+                                                                <div className="flex flex-row">
+                                                                    <h1>{sub.barang}</h1>
+                                                                </div>
+                                                            </>
+                                                        )
+                                                    })
+                                                }
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </>
     );
