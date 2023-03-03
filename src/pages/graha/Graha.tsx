12eae51efd536705fcc344d45b2428926f256c85
Natabagas <meganatabagas@gmail.com>
2023-03-03 11:29:18 +0700

    Restyling graha

--- src/pages/graha/Graha.tsx
+++ src/pages/graha/Graha.tsx
@@ -4,6 +4,8 @@ import fotoGedung from '../../assets/gedung.jpg'
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { useNavigate } from "react-router-dom";
 import Footer from "../dashboard/utils/Footer";
+import {GrLocation} from 'react-icons/gr'
+import { useEffect, useState } from "react";
 
 const Graha = () => {
     const navigate = useNavigate()
@@ -15,19 +17,19 @@ const Graha = () => {
                     {
                         gedung.map((data, index) => {
                             return (
-                                <div key={index} onClick={() => navigate(`/graha/${data.id}`)} className="mr-[20px] hover:opacity-80 cursor-pointer hover:-translate-y-2 transition rounded-xl border my-[30px] bg-white">
+                                <div key={index} onClick={() => navigate(`/graha/${data.id}`)} className="mr-[20px] hover:opacity-80 shadow-md outline-none cursor-pointer hover:-translate-y-2 transition rounded-xl border my-[30px] bg-white">
                                     <div>
-                                        <LazyLoadImage className="w-[400px] rounded-t-xl" src={fotoGedung} alt="Foto gedung" />
+                                        <LazyLoadImage className="w-full h-[250px] rounded-t-xl" src={fotoGedung} alt="Foto gedung" />
                                     </div>
-                                    <div className="bg-[#f0f0f0] py-[20px] font-inter text-black rounded-b-xl w-full">
+                                    <div className="bg-white py-[10px] font-inter text-black rounded-b-xl w-full">
                                         <div className="mx-[20px]">
                                             <h1 className="text-[18px] font-bold">{data.name}</h1>
-                                            <div className="my-8">
+                                            <div className="my-5 font-inter text-[12px]">
                                                 <h3>{data.alamat}</h3>
-                                                <h3 className="mt-3">{data.kecamatan}</h3>
+                                                <h3 className="mt-1 text-[#6A7682] flex flex-row items-center"><span className="mr-2"><GrLocation/></span>{data.kecamatan}</h3>
                                             </div>
                                             <div className="flex flex-col">
-                                                <h3 className="text-[10px]">Mulai Dari</h3>
+                                                <h3 className="text-[10px] text-[#1B1D21]">Mulai Dari</h3>
                                                 <h1 className="text-[16px] font-bold">{data.harga}</h1>
                                             </div>
                                         </div>
--- src/pages/graha/feature/Booking.tsx
+++ src/pages/graha/feature/Booking.tsx
@@ -1,4 +1,6 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import {IoIosArrowForward} from 'react-icons/io'
+import {GrLocation} from 'react-icons/gr'
 import { gedung } from '../../../models/dummy/gedung'
 import Nav from "../../dashboard/utils/Nav";
 import Ruang1 from '../../../assets/gedung.jpg'
@@ -6,9 +8,11 @@ import Ruang2 from '../../../assets/gedung2.jpeg'
 import Ruang3 from '../../../assets/gedung1.jpg'
 import { useEffect, useState } from "react";
 import Button from "../../../components/button/Button";
+import { useNavigate } from "react-router-dom";
 
 const Booking = () => {
     const [nama, setNama] = useState('')
+    const navigate = useNavigate()
     useEffect(() => {
         gedung.filter(data => {
             if (data.id === 1) {
@@ -19,7 +23,12 @@ const Booking = () => {
     return (
         <>
             <Nav />
-            <div className="mx-[50px] my-[150px]">
+            <div className="mx-[50px] my-[100px]">
+                <div className="my-[30px] items-center font-inter flex flex-row">
+                    <h1 className="opacity-[50%]">Graha &nbsp;</h1>
+                    <IoIosArrowForward className="opacity-[50%]"/> 
+                    <span className="font-bold text-black opacity-[100%]">&nbsp; {nama}</span>
+                </div>
                 <div className="flex items-stretch justify-between flex-row">
                     <LazyLoadImage
                         src={Ruang1}
@@ -43,7 +52,7 @@ const Booking = () => {
                         </div>
                     </div>
                 </div>
-                <div className="mt-[100px]">
+                <div className="mt-[20px]">
                     {
                         gedung.filter(data => {
                             if (data.id === 1) {
@@ -55,8 +64,25 @@ const Booking = () => {
                                     <div className="flex flex-row justify-between ">
                                         <div className="flex flex-col border rounded-xl p-5 font-inter w-[64%]">
                                             <h1 className="font-inter font-bold text-[32px]">{sub.name}</h1>
-                                            <h3 className="text-[20px] font-semibold">{sub.alamat}</h3>
-                                            <div className="mt-[100px]">
+                                            <h3 className="text-[18px]">{sub.alamat}</h3>
+                                            <h3 className="mt-2 text-[#6A7682] opacity-[50%] flex flex-row items-center"><span className="mr-2"><GrLocation/></span>{sub.kecamatan}</h3>
+                                            <div className="mt-[30px]">
+                                                <h1 className="font-bold text-[20px] mt-5">Fasilitas : </h1>
+                                            </div>
+                                            {
+                                                sub.fasilitas.map((fasil) => {
+                                                    return (
+                                                        <div>
+                                                            <ul>
+                                                                <li className="ml-[100px]">
+                                                                    <span className="font-bold">{fasil.id}.</span> {fasil.barang}
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                            <div className="mt-[50px]">
                                                 <h1 className="font-bold text-[20px]">Spesifikasi Luas : <span className="font-medium text-[16px]">{sub.Luas}</span></h1>
                                                 <h1 className="font-bold text-[20px] mt-5">Kapasitas : <span className="font-medium text-[16px]">{sub.kapasitas}</span></h1>
                                                 <h1 className="font-bold text-[20px] mt-5">Aturan : </h1>
@@ -74,31 +100,22 @@ const Booking = () => {
                                                     )
                                                 })
                                             }
-                                            <div>
-                                                <h1 className="font-bold text-[20px] mt-5">Fasilitas : </h1>
-                                            </div>
-                                            {
-                                                sub.fasilitas.map((fasil) => {
-                                                    return (
-                                                        <div>
-                                                            <ul>
-                                                                <li className="ml-[100px]">
-                                                                    <span className="font-bold">{fasil.id}.</span> {fasil.barang}
-                                                                </li>
-                                                            </ul>
-                                                        </div>
-                                                    )
-                                                })
-                                            }
                                         </div>
                                         <div className='w-[35%] ml-5'>
                                             <div className="border top-24 sticky rounded-xl p-5">
-                                                <h1 className="text-[20px] font-bold">Harga Mulai Dari</h1>
-                                                <h3 className="font-medium">{sub.harga}</h3>
+                                                <h1 className="text-[16px] ">Mulai Dari</h1>
+                                                <h3 className="font-bold text-[25px] mt-2">{sub.harga}</h3>
+
+                                                <Button
+                                                    type="button"
+                                                    className="mt-10 w-full text-[#F78CB2] bg-white border border-[#F78CB2]"
+                                                    children="Tanya Pemilik"
+                                                    onClick={() => navigate('/')}
+                                                />
 
                                                 <Button
                                                     type="submit"
-                                                    className="mt-10 w-full text-white bg-[#F78CB2] hover:bg-[#f379a3]"
+                                                    className="mt-5 w-full text-white bg-[#F78CB2] hover:bg-[#f379a3]"
                                                     children="Booking Sekarang"
                                                 />
                                             </div>
